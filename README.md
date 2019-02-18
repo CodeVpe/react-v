b220a790ca705e3afcff89d86cc20909b0d2aafe
CodeVpe <120424596@qq.com>
2019-02-18 20:55:11 +0800

    dsds

---
+++ README.md
@@ -0,0 +1,35 @@
+### 从零开始使用React全家桶开发电商系统
+
+### 技术栈：
+react + react-router4.1 + redux + koa2 + koa-router + less + ES6/7 + webpack + fetch + bundle-loader
+
+### 项目说明：
+
+### 基于 react react-router redux 的项目，主要是为了学习实战react全家桶
+
+### 下载
+
+ 	git clone 
+
+ 	cd dianping
+
+ 	npm install
+
+### 运行（nodejs 7.6.0+）
+```
+ npm run dev (正常编译模式)
+
+ 访问 http://localhost:8081
+
+ npm run build （发布生产版本，对代码进行混淆压缩，提取公共代码，分离css文件）
+```
+
+
+## 附项目截图
+
+![image](./images/1.jpg)
+
+![image](./images/2.jpg)
+
+![image](./images/3.jpg)
+
---
+++ mockServer/home/ad.js
@@ -0,0 +1,33 @@
+var config=require("../config/config")
+module.exports = [
+    {
+        title: '暑假5折',
+        img:  config.imgUrl+'/2.png',
+        link: '#'
+    },
+    {
+        title: '特价出国',
+        img: config.imgUrl+'/3.png',
+        link: '#'
+    },
+    {
+        title: '亮亮车',
+        img:  config.imgUrl+'/4.png',
+        link: '#'
+    },
+    {
+        title: '学钢琴',
+        img:  config.imgUrl+'/5.png',
+        link: '#'
+    },
+    {
+        title: '电影',
+        img:  config.imgUrl+'/6.png',
+        link: '#'
+    },
+    {
+        title: '旅游热线',
+        img:  config.imgUrl+'/7.png',
+        link: '#'
+    }
+]
